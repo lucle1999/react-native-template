17138f0ba814600f44c331be81899f8b828ef1ce
Le Tan Luc <lucle.26041999@gmail.com>
2022-03-02 23:10:16 +0700

    error handler

--- template/App.js
+++ template/App.js
@@ -2,13 +2,51 @@ import MyStack from "navigation";
 import React from "react";
 import { Provider } from "react-redux";
 import createStore from "./src/store";
+import {
+  setJSExceptionHandler,
+  setNativeExceptionHandler
+} from "react-native-exception-handler";
+import { Alert } from "react-native";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, ModalService } from "@ui-kitten/components";
 
 const store = createStore();
 
+const errorHandler = (e, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+      "Có lỗi xảy ra",
+      `
+        Error: ${isFatal ? "Fatal:" : ""} ${e.name} ${e.message}
+        Chúng tôi đã báo lỗi này cho đội ngũ phát triển! Vui lòng đóng ứng dụng và mở lại!
+        `,
+      [
+        {
+          text: "Đóng",
+          onPress: () => {}
+        }
+      ]
+    );
+  } else {
+    console.log(e);
+  }
+};
+
+const nativeErrorHandler = (errorString) => {
+  console.log(errorString);
+};
+
+setJSExceptionHandler(errorHandler, true);
+setNativeExceptionHandler(nativeErrorHandler);
+
+ModalService.setShouldUseTopInsets = true;
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <MyStack />
-    </Provider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Provider store={store}>
+        <MyStack />
+      </Provider>
+    </ApplicationProvider>
   );
 }
